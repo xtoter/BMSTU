c1b80a8e9226477a15a2d7fdd8d30a2a086c446f
TER <misha37a999@yandex.ru>
2021-06-03 01:04:37 +0300

    OpenCv append

---
+++ c++(2 семестр)/OpenCVlabs/HandPose(with cam)/handPoseImage.cpp
@@ -0,0 +1,158 @@
+#include <opencv2/dnn.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+// rapidjson/example/simpledom/simpledom.cpp`
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <iostream>
+using namespace rapidjson;
+using namespace std;
+using namespace cv;
+using namespace cv::dnn;
+
+
+const int POSE_PAIRS[20][2] =
+        {
+                {0,1}, {1,2}, {2,3}, {3,4},         // thumb
+                {0,5}, {5,6}, {6,7}, {7,8},         // index
+                {0,9}, {9,10}, {10,11}, {11,12},    // middle
+                {0,13}, {13,14}, {14,15}, {15,16},  // ring
+                {0,17}, {17,18}, {18,19}, {19,20}   // small
+        };
+
+string protoFile = "hand/pose_deploy.prototxt";
+string weightsFile = "hand/pose_iter_102000.caffemodel";
+
+int nPoints = 22;
+void serializeToJson( const std::vector<Point> & myObjs) {
+    rapidjson::Document jsonDoc;
+    jsonDoc.SetArray();
+    rapidjson::Document::AllocatorType& allocator = jsonDoc.GetAllocator();
+    rapidjson::Value koorsArray(rapidjson::kArrayType);
+    for (auto & koord : myObjs) {
+        rapidjson::Value objKoord;
+        objKoord.SetObject();
+        objKoord.AddMember("x", koord.x, allocator);
+        objKoord.AddMember("y", koord.y, allocator);
+        koorsArray.PushBack(objKoord, allocator);
+    }
+    rapidjson::Value objValue;
+    objValue.SetObject();
+    objValue.AddMember("coordinat", koorsArray, allocator);
+    jsonDoc.PushBack(objValue, allocator);
+
+    rapidjson::StringBuffer strbuf;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(strbuf);
+    jsonDoc.Accept(writer);
+    cout << strbuf.GetString() << endl;
+}
+int main(int argc, char **argv)
+{
+
+
+    VideoCapture cam{0};
+
+    if (cam.isOpened())
+    {
+        String window = "Mirrow";
+
+        namedWindow(window);
+
+        auto run = true;
+        Mat img;
+        while(run)
+        {
+
+
+            cam >> img; // получить кадр
+            cout << "new image " << endl;
+
+//
+
+            imshow(window, img);
+            float thresh = 0.01;
+
+            Mat frame = img;
+            Mat frameCopy = frame.clone();
+            int frameWidth = frame.cols;
+            int frameHeight = frame.rows;
+
+            float aspect_ratio = frameWidth/(float)frameHeight;
+            int inHeight = 368;
+            int inWidth = (int(aspect_ratio*inHeight) * 8) / 8;
+
+            cout << "inWidth = " << inWidth << " ; inHeight = " << inHeight << endl;
+
+            double t = (double) cv::getTickCount();
+            Net net = readNetFromCaffe(protoFile, weightsFile);
+
+            Mat inpBlob = blobFromImage(frame, 1.0 / 255, Size(inWidth, inHeight), Scalar(0, 0, 0), false, false);
+
+            net.setInput(inpBlob);
+
+            Mat output = net.forward();
+
+            int H = output.size[2];
+            int W = output.size[3];
+
+            // find the position of the body parts
+            vector<Point> points(nPoints);
+            for (int n=0; n < nPoints; n++)
+            {
+                // Probability map of corresponding body's part.
+                Mat probMap(H, W, CV_32F, output.ptr(0,n));
+                resize(probMap, probMap, Size(frameWidth, frameHeight));
+
+                Point maxLoc;
+                double prob;
+                minMaxLoc(probMap, 0, &prob, 0, &maxLoc);
+                if (prob > thresh)
+                {
+                    circle(frameCopy, cv::Point((int)maxLoc.x, (int)maxLoc.y), 8, Scalar(0,255,255), -1);
+                    cv::putText(frameCopy, cv::format("%d", n), cv::Point((int)maxLoc.x, (int)maxLoc.y), cv::FONT_HERSHEY_COMPLEX, 1, cv::Scalar(0, 0, 255), 2);
+
+                }
+                points[n] = maxLoc;
+            }
+
+            int nPairs = sizeof(POSE_PAIRS)/sizeof(POSE_PAIRS[0]);
+
+            serializeToJson(points);
+            for (int n = 0; n < nPairs; n++)
+            {
+                // lookup 2 connected body/hand parts
+                Point2f partA = points[POSE_PAIRS[n][0]];
+                Point2f partB = points[POSE_PAIRS[n][1]];
+
+                if (partA.x<=0 || partA.y<=0 || partB.x<=0 || partB.y<=0)
+                    continue;
+
+                line(frame, partA, partB, Scalar(0,255,255), 8);
+                circle(frame, partA, 8, Scalar(0,0,255), -1);
+                circle(frame, partB, 8, Scalar(0,0,255), -1);
+            }
+
+            t = ((double)cv::getTickCount() - t)/cv::getTickFrequency();
+            //cout << frameCopy[0] << endl;
+
+            cout << "Time Taken = " << t << endl;
+
+                imshow(window, frame);
+
+            /*
+            imshow("Output-Keypoints", frameCopy);
+            imshow("Output-Skeleton", frame);
+            imwrite("Output-Skeleton.jpg", frame);
+            waitKey();*/
+//
+            run = (waitKey(33) != 27);
+        }
+
+        destroyAllWindows();
+    }
+
+
+    return 0;
+}
